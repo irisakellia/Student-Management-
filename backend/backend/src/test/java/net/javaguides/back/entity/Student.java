6d040c0e2f81c6e8db035843d56a9ca650f24fbf
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-10-15 07:26:18 +0200

    [added] to a file

--- backend/backend/src/test/java/net/javaguides/back/entity/Student.java
+++ backend/backend/src/test/java/net/javaguides/back/entity/Student.java
@@ -14,19 +14,22 @@ import lombok.Setter;
 @Table(name = "students")
 public class Student {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "photo_url")
     private String photourl;
+
     @Column(name = "first_name")
     private String firstName;
+
     @Column(name = "last_name")
-    private String LastName;
+    private String lastName; // Corrected case of "lastName"
+
     @Column(name = "email", nullable = false, unique = true)
     private String email;
+
     @Column(name = "gender")
     private String gender;
-
 }

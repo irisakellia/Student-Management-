92e1216d8595e84f7de16143d3de0d18783e0212
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-10-16 09:08:53 +0200

    [added] changes

--- frontend/Student/src/components/ListStudentComponent.jsx
+++ frontend/Student/src/components/ListStudentComponent.jsx
@@ -1,52 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { listStudents } from '../service/StudentService'
-
-
-const [students , setStudents ] = useState([])
-
-useEffect(()=>{
-
-    listStudents().then((response)=>{
-        setStudents(response.data);
-    }).catch(error=>{
-        console.error(error)
-    })
-},[])
-
+import React, { useEffect, useState } from 'react';
+import { listStudents } from '../service/StudentService';
 
 const ListStudentComponent = () => {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    listStudents()
+      .then((response) => {
+        setStudents(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div>
-      <table>
+      <table className="table-auto">
         <thead>
-            <tr>
-                <th>Photo</th>
-                <th>FirstName</th>
-                <th>LastName</th>
-                <th>Email</th>
-                <th>Gender</th>
-            </tr>
+          <tr>
+            <th>Photo</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+          </tr>
         </thead>
-        <tbody>{
-
-            students.map(student =>
-                <tr key={student.id}>
-                <td>{student.id}</td>
-                <td>{student.photo}</td>
-                <td>{student.firstName}</td>
-                <td>{student.lastName}</td>
-                <td>{student.Email}</td>
-                <td>{student.gender}</td>
-             </tr>
-
-            )
-           
-            }
-           
+        <tbody>
+          {students.map((student) => (
+            <tr key={student.id}>
+              <td>
+                <img src={student.photo_url} alt={`${student.firstName} ${student.lastName}`} width="50" height="50" />
+              </td>
+              <td>{student.firstName}</td>
+              <td>{student.lastName}</td>
+              <td>{student.email}</td> {/* Corrected to "email" */}
+              <td>{student.gender}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default ListStudentComponent
+export default ListStudentComponent;
